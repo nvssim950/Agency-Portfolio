660ec3d6fc6c587d92ab8bbfbcffeb364858c25d
NASSIM BAHEDDI <nassimbddm@gmail.com>
2025-07-27 23:57:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "NVS Agency",
+  other: {
+    'google-adsense-account': 'ca-pub-1479410692669527'
+  },
   description: "NVS Agency is a professional web development and business automation company specializing in modern digital solutions. We create high-performance websites, scalable SaaS platforms, and efficient dashboards using technologies like Next.js, Tailwind CSS, and React. Our automation services use Python and N8N to eliminate manual tasks and streamline business operations. With a proven track record of delivering user-friendly, SEO-optimized, and responsive web projects, NVS helps clients increase productivity, reduce costs, and stand out in today’s competitive digital landscape.",
 };
 
